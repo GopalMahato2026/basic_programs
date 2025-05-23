b2f5deb266e9ad2daf46e2a0b4c89bded838aca0
GopaLMahato2026 <gopalmahato0701@gmail.com>
2025-05-23 19:02:39 +0530

    2nd version of exception

--- error_handling/exceptions.py
+++ error_handling/exceptions.py
@@ -17,8 +17,34 @@ try:
 except NameError as ex:
     print(ex)
 """
+"""
 try:
     result = 1 / 0
 except ZeroDivisionError as ex:
     print("Please enter the demoninator grater then zero")
+"""
+"""
+try:
+    num = float(input("Enter a number: "))
+    result = 10 / num
+except ValueError:
+    print("This is not a valid number")
+except ZeroDivisionError:
+    print("Enter denominator greater than zero")
+except Exception as ex:
+    print(ex)
+else:
+    print(result)
+finally:
+    print("Execution complete!")
     
+ """  
+ ## file handling and exception handling
+try:
+    with open("example1.txt", "r") as file:
+        content = file.read()
+        print(content)
+except FileNotFoundError:
+    print("The file doesn't exists")
+finally:
+    print("file closed")
